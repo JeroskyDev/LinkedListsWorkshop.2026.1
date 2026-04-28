d2ac193ea6842fc8e5f53451e37ec60c7dcdda34
JERONIMORAMIREZRESTREPO <jeronimoramirez1159288@correo.itm.edu.co>
2026-04-28 17:40:47 -0500

    Insert starting files and make inicial structure for all of the files. Finished Nodes btw.

---
+++ DoubleLinkedListsSolution/DLL.Backend/DoubleLinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DLL.Backend;
+
+public class DoubleLinkedList<T>
+{
+    //fields
+    private Node<T>? _head;
+    private Node<T>? _tail;
+
+    //constructor
+    public DoubleLinkedList()
+    {
+        _head = null;
+        _tail = null;
+    }
+
+    //methods to do:
+    public void Add()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ShowForward()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ShowBackwards()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OrderDescending()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ShowMode()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string ShowGraph()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Exists()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteOcurrence()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAllOcurrences()
+    {
+        throw new NotImplementedException();
+    }
+
+    //private methods
+    override public string ToString()
+    {
+        return base.ToString();
+    }
+}
---
+++ DoubleLinkedListsSolution/DLL.Frontend/Program.cs
@@ -0,0 +1,19 @@
+﻿//console app for our double linked list.
+
+var list = DoubleLinkedList<int>();
+var option = string.Empty;
+var value = string.Empty;
+
+do
+{
+    option = Menu();
+    switch(option)
+    {
+
+    }
+} while (option != "0");
+
+string Menu()
+{
+    throw new NotImplementedException();
+}
